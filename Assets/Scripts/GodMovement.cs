1b48e73609b402b4b16c637ecc7ec9987bca2dac
Yagi2001 <ramazanbatu2001@outlook.com>
2024-09-17 22:49:58 +0300

    ArenaAndPortalSceneAdded

--- Assets/Scripts/GodMovement.cs
+++ Assets/Scripts/GodMovement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GodMovement : MonoBehaviour
 {
     [SerializeField]
     private Transform _playerPosition;
-
+    public static Action ForestTalkFinished;
+    private float _testTimer;
     private void OnEnable()
     {
         GameController.firstMeetingWithGod += CharacterAppear;
@@ -17,6 +20,14 @@ public class GodMovement : MonoBehaviour
         GameController.firstMeetingWithGod -= CharacterAppear;
     }
 
+    private void Update()
+    {
+        _testTimer += Time.deltaTime;
+        if (_testTimer >= 17f)
+        {
+            SceneManager.LoadScene( "PortalScene" );
+        }
+    }
 
     public void CharacterAppear()
     {
