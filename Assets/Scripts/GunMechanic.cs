eb55ad9abdb01d2eaca74ece75e45ac315955646
Yagi2001 <ramazanbatu2001@outlook.com>
2024-10-20 23:28:14 +0300

    HitEffectandFireRateAdded

--- Assets/Scripts/GunMechanic.cs
+++ Assets/Scripts/GunMechanic.cs
@@ -7,14 +7,21 @@ public class GunMechanic : MonoBehaviour
     [SerializeField]
     private float _range = 100f;
     [SerializeField]
+    private float _fireRate = 15f;
+    [SerializeField]
     private ParticleSystem _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitEffect;
     [SerializeField]
     private Camera _fpsCam;
 
+    private float nextTimeToFire =0f;
+
     private void Update()
     {
-        if (Input.GetButtonDown( "Fire1" ))
+        if (Input.GetButtonDown( "Fire1" ) && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / _fireRate;
             Shoot();
         }
     }
@@ -30,6 +37,11 @@ public class GunMechanic : MonoBehaviour
             EnemyDamage enemyDamage = hitInfo.transform.GetComponent<EnemyDamage>();
             if (enemyDamage != null)
                 enemyDamage.TakeDamage( _damage );
+        }
+        if (_hitEffect != null)                 //Need to find an effect for here
+        {
+            GameObject hitEffectGO =  Instantiate( _hitEffect, hitInfo.point, Quaternion.LookRotation( hitInfo.normal ) );
+            Destroy( hitEffectGO, 2f );
         }
 
     }
