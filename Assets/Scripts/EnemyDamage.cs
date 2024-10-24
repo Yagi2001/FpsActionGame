e95c00fc2e35cb43a387bd265eef147dbdc66ce5
Yagi2001 <ramazanbatu2001@outlook.com>
2024-10-24 22:27:44 +0300

    ZombieAnimationImprovements

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -10,20 +10,32 @@ public class EnemyDamage : MonoBehaviour
     private Animator _anim;
     [SerializeField]
     private Rigidbody _rb;
+    private bool _isDying;
+
+    private void Start()
+    {
+        _isDying = false;
+    }
+
     public void TakeDamage(float damage)
     {
         _health -= damage;
         if (_health <= 0f)
             Die();
+        else
+        {
+            _anim.SetTrigger( "Hit1Trigger" );
+        }
     }
 
     private void Die()
     {
-        _rb.isKinematic = true;   //This is a solution for weird flying dead bug. Later can be changed to an better alternative
-        if(_anim != null)
+        if(_anim != null && !_isDying)
         {
+            _isDying = true;
+            _rb.isKinematic = true;   //This is a solution for weird flying dead bug. Later can be changed to an better alternative
             _anim.SetTrigger( "DeathTrigger" );
+            Destroy( gameObject, 5f );
         }
-        Destroy( gameObject,5f );
     }
 }
